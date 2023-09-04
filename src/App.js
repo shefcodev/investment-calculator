fde94d1dc5740e8b68f86e3171ca459ba7b06a4f
shefcodev <sherefjimoh@gmail.com>
2023-09-04 18:59:59 +0100

    rendered investment results

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
+import React, { useState } from 'react';
 import logo from './assets/investment-calculator-logo.png';
 import Header from './components/Investment/Header';
 import InvData from './components/Investment/InvData';
-import Result from './components/Result/Result';
+import Results from './components/Result/Results';
 
 const App = () => {
-  const calculateHandler = (userInput) => {
+  const [invData, setInvData] = useState([]);
 
-    const yearlyData = []; 
+  const calculateHandler = (userInput) => {
+    const yearlyData = [];
 
     let currentSavings = +userInput['current-savings'];
     const yearlyContribution = +userInput['yearly-contribution'];
     const expectedReturn = +userInput['expected-return'] / 100;
     const duration = +userInput['duration'];
+    let totalInterest = 0;
 
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
+      totalInterest += yearlyInterest;
       currentSavings += yearlyInterest + yearlyContribution;
       yearlyData.push({
+        key: i,
         year: i + 1,
-        yearlyInterest: yearlyInterest,
-        savingsEndOfYear: currentSavings,
+        yearlyInterest: yearlyInterest.toFixed(2),
+        savingsEndOfYear: currentSavings.toFixed(2),
+        totalInterest: totalInterest.toFixed(2),
+        investCapital: (currentSavings - totalInterest).toFixed(2),
         yearlyContribution: yearlyContribution,
       });
     }
 
+    setInvData(yearlyData);
+    console.log(invData);
   };
 
   return (
     <div>
       <Header src={logo} alt='logo' />
-      <InvData />
-      <Result />
+      <InvData calculateHandler={calculateHandler} />
+      <Results results={invData}/>
     </div>
   );
 };
--- src/components/Investment/InvData.js
+++ src/components/Investment/InvData.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Buttons from '../UI/Buttons/Buttons';
 import styles from './InvData.module.css';
 
-const InvData = () => {
+const InvData = ({ calculateHandler }) => {
+  const [invValues, setInvValues] = useState({});
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    calculateHandler(invValues);
+  };
+
+  const resetHandler = () => {
+    setInvValues({});
+  }
+
+  const inputHandler = ({ target: { id, value } }) => {
+    setInvValues((prevValue) => {
+      return { ...prevValue, [id]: value };
+    });
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler} onReset={resetHandler}>
       <div className={styles['input-group']}>
         <p>
           <label htmlFor='current-savings'>Current Savings ($)</label>
-          <input type='number' id='current-savings' />
+          <input type='number' id='current-savings' onChange={inputHandler} />
         </p>
         <p>
           <label htmlFor='yearly-contribution'>Yearly Savings ($)</label>
-          <input type='number' id='yearly-contribution' />
+          <input
+            type='number'
+            id='yearly-contribution'
+            onChange={inputHandler}
+          />
         </p>
       </div>
       <div className={styles['input-group']}>
@@ -20,11 +41,11 @@ const InvData = () => {
           <label htmlFor='expected-return'>
             Expected Interest (%, per year)
           </label>
-          <input type='number' id='expected-return' />
+          <input type='number' id='expected-return' onChange={inputHandler} />
         </p>
         <p>
           <label htmlFor='duration'>Investment Duration (years)</label>
-          <input type='number' id='duration' />
+          <input type='number' id='duration' onChange={inputHandler} />
         </p>
       </div>
       <Buttons />
